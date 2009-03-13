75de49ba85fe15379a97920efc4e4790ee003fc0
blaine <blaine@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-03-13 03:12:42 +0000

    _start must load the new files
    git-svn-id: http://subversion.assembla.com/svn/codetodiagram@22 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- components/xmlSequence/_start.php
+++ components/xmlSequence/_start.php
@@ -1,8 +1,7 @@
 <?php
-
+require_once( 'XmlSequenceException.class.php' );
 require_once( 'XmlSequenceActor.class.php' );
 require_once( 'XmlSequenceMessage.class.php' );
 require_once( 'XmlSequenceValue.class.php' );
 require_once( 'XmlSequence.class.php' );
-
 ?>
