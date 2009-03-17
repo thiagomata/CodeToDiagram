c037bd7f893d622a02dd50203bf93d5176cf5f94
blaine <blaine@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-03-17 04:41:57 +0000

    tree little pigs example updgraded
    git-svn-id: http://subversion.assembla.com/svn/codetodiagram@29 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- examples/TreeLittlePigs/treeLittlePigs.php
+++ examples/TreeLittlePigs/treeLittlePigs.php
@@ -1,8 +1,6 @@
 <?php
-/** DEBUG_IGNORE **/
 $strCodeToDiagramOutputFile="./anyname.html";
 require_once( '../../public/codetodiagram.php' );
-/** END_DEBUG_IGNORE **/
 
 require_once( 'Wolf.class.php' );
 require_once( 'Pig.class.php' );
