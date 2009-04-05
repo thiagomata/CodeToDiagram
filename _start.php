e3750154476f52b0eac632510b87c2fca96fedc4
blaine <blaine@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-05 13:36:10 +0000

    wrong const name
    git-svn-id: http://subversion.assembla.com/svn/codetodiagram@31 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- _start.php
+++ _start.php
@@ -1,7 +1,7 @@
 <?php
 
 define( "PATH_CODE_TO_DIAGRAM" , str_replace( "\\" , "/" , str_replace( basename( __FILE__ ) , "" , __FILE__ ) ) );
-define( "PATH_CODE_TO_DIAGRAM_COMPONENT" , PATH_CORUJA . "components/" );
+define( "PATH_CODE_TO_DIAGRAM_COMPONENT" , PATH_CODE_TO_DIAGRAM . "components/" );
 
 require_once( PATH_CODE_TO_DIAGRAM_COMPONENT . "_start.php" );
 
