3e1017ec37244c20cd943c8ad1219117fa872dc8
blaine <blaine@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-13 01:24:48 +0000

    break the big method of the code to diagram into many small methods
    git-svn-id: http://subversion.assembla.com/svn/codetodiagram@39 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- components/codeToDiagram/CodeToDiagram.class.php
+++ components/codeToDiagram/CodeToDiagram.class.php
@@ -33,6 +33,11 @@ class CodeToDiagram
                 $this->strOutputType = $strType;
                 break;
             }
+            default:
+            {
+                throw new CodeToDiagramException( "Invalid output type. ('" . $strType . "')" );
+                break;
+            }
         }
     }
 
@@ -62,6 +67,7 @@ class CodeToDiagram
     public function setStarted( $booStarted )
     {
         $this->booStart = $booStarted;
+        return $this;
     }
 
     public function getStarted()
@@ -72,6 +78,7 @@ class CodeToDiagram
     public function setFileFrom( $strFileFrom )
     {
         $this->strFileFrom = $strFileFrom;
+        return $this;
     }
 
     public function getFileFrom()
@@ -82,6 +89,7 @@ class CodeToDiagram
     public function setFileName( $strFileName )
     {
         $this->strFileName = $strFileName;
+        return $this;
     }
 
     public function getFileName()
@@ -137,6 +145,7 @@ class CodeToDiagram
     public function restart()
     {
         CodeInstrumentationReceiver::getInstance()->restart();
+        return $this;
     }
 
     public function save()
@@ -155,6 +164,7 @@ class CodeToDiagram
             }
             CodeInstrumentationReceiver::getInstance()->restart();
         }
+        return $this;
     }
 
     public function  __destruct() {
@@ -185,6 +195,8 @@ class CodeToDiagram
         $strFile = $this->fixFileName( $strFileFrom, $strFile );
 
         $this->arrFiles[] = $strFile;
+
+        return $this;
     }
 
     public function hasFile( $strFileFrom , $strFile )
@@ -194,15 +206,13 @@ class CodeToDiagram
 
     public function CodeToDiagramRequireOnce( $strFileFrom, $strFile )
     {
-//        print "CodeToDiagramRequireOnce from: "  . $strFileFrom . "<Br/>\n";
-//        print "CodeToDiagramRequireOnce to:  " . $strFile . "<Br/>\n";
-
         $arrCodeToDiagramBackTrace = debug_backtrace();
 
         if( !$this->hasFile( $strFileFrom , $strFile ) )
         {
             $this->CodeToDiagramRequire( $strFileFrom, $strFile );
         }
+        return $this;
     }
 
     public function CodeToDiagramIncludeOnce( $strFileFrom, $strFile )
@@ -211,16 +221,19 @@ class CodeToDiagram
         {
             $this->CodeToDiagramIncludeOnce( $strFileFrom , $strFile );
         }
+        return $this;
     }
 
     public function CodeToDiagramRequire( $strFileFrom, $strFile )
     {
         $this->loadFile( $strFileFrom , $strFile );
+        return $this;
     }
 
     public function CodeToDiagramInclude( $strFileFrom, $strFile )
     {
         $this->loadFile( $strFileFrom , $strFile );
+        return $this;
     }
 
     public function CodeToDiagramExit( $strFileFrom, $strMessage = '')
@@ -229,35 +242,8 @@ class CodeToDiagram
         exit();
     }
 
-    public function loadFile( $strFileFrom, $strFile )
+    public function convertFileContent( $strContentFile , $strFile , $strFullFile )
     {
-//        print "loadfile from: "  . $strFileFrom . "<Br/>\n";
-//        print "loadfile to:  " . $strFile . "<Br/>\n";
-//        print_r( $this->arrFiles );
-        
-        if( basename( $strFile ) == 'CodeToDiagram.class.php' )
-        {
-            return;
-        }
-        $this->addFile( $strFileFrom, $strFile );
-
-        if( $this->isRelativePath( $strFile ) )
-        {
-            $strFullFile = $this->fixFileName( $strFileFrom, $strFile );
-        }
-        else
-        {
-            $strFullFile = $strFile;
-        }
-
-        if( ! file_exists( $strFullFile ) )
-        {
-            // @todo Catch this exception
-            throw new CodeToDiagramException( "Unable to find file: " . $strFullFile );
-        }
-
-        $strContentFile = file_get_contents( $strFullFile );
-
         if( self::getInstance()->getStarted() == false )
         {
             self::getInstance()->setStarted( true );
@@ -286,63 +272,60 @@ class CodeToDiagram
             $strContentFile
         );
 
-        $arrLines = explode( "\n"  , $strContentFile );
+        return $strContentFile;
+    }
 
+    public function codeInstrumentationLine( $strLine, $strTextSearch )
+    {
+        $arrResult = array();
+
+        if(  substr( strtolower( trim( $strLine ) ) , 0 , strlen( $strTextSearch ) ) == $strTextSearch )
+        {
+            $strLine = trim( $strLine );
+            $strBefore = substr( $strLine , 0 , strlen( $strTextSearch ) );
+            $strAfter = substr( $strLine , strlen( $strTextSearch ) );
+            $arrWords = explode( " " , $strAfter );
+
+            $strOldClassName = $arrWords[0];
+            $strNewClassName = self::CODE_TO_DIAGRAM_CLASS_PREFIX . $strOldClassName;
+
+            $arrWords[0] = $strNewClassName;
+            $strAfter = implode( " " , $arrWords );
+            $strLine = $strBefore . $strAfter;
+
+            $arrResult[ "line" ] = $strLine;
+            $arrResult[ "class_old" ] = $strOldClassName;
+            $arrResult[ "class_new" ] = $strNewClassName;
+
+        }
+        return $arrResult;
+    }
+
+    protected function codeInstrumentationClass( $strTextSearch, array &$arrOldClasses , array &$arrNewClasses , array &$arrLines )
+    {
+        foreach( $arrLines as $intLine => $strLine )
+        {
+            $arrResult = $this->codeInstrumentationLine( $strLine , $strTextSearch  );
+
+            if( sizeof( $arrResult ) !== 0 )
+            {
+                $arrOldClasses[] = $arrResult[ "class_old" ];
+                $arrNewClasses[] = $arrResult[ "class_new" ];
+                $arrLines[ $intLine ] = $arrResult[ "line" ];
+            }
+        }
+    }
+
+    public function codeInstrumentationFile( $strFile , $strContentFile )
+    {
+        $arrLines = explode( "\n"  , $strContentFile );
         $arrOldClasses   = array();
         $arrNewClasses   = array();
         $arrOldInterface = array();
         $arrNewInterface = array();
 
-        foreach( $arrLines as $intLine => $strLine )
-        {
-            $strTextSearch = "class ";
-            if(  substr( strtolower( trim($strLine) ) , 0 , strlen( $strTextSearch ) ) == $strTextSearch )
-            {
-                $strLine = trim($strLine);
-                $strBefore = substr( $strLine , 0 , strlen( $strTextSearch ) );
-                $strAfter = substr( $strLine , strlen( $strTextSearch ) );
-                $arrWords = explode( " " , $strAfter );
-
-                $strOldClassName = $arrWords[0];
-                $strNewClassName = self::CODE_TO_DIAGRAM_CLASS_PREFIX . $strOldClassName;
-
-                $arrOldClasses[] = $strOldClassName;
-                $arrNewClasses[] = $strNewClassName;
-
-                $arrWords[0] = $strNewClassName;
-                $strAfter = implode( " " , $arrWords );
-                $strLine = $strBefore . $strAfter;
-                $arrLines[ $intLine ] = $strLine;
-            }
-
-            $strTextSearch = "interface ";
-            if(  substr( strtolower( trim($strLine) ) , 0 , strlen( $strTextSearch ) ) == $strTextSearch )
-            {
-                //                $strLine = trim($strLine);
-                $strBefore = substr( $strLine , 0 , strlen( $strTextSearch ) );
-                $strAfter = substr( $strLine , strlen( $strTextSearch ) );
-                $arrWords = explode( " " , $strAfter );
-
-                /**
-                 * @todo
-                 * Estou procurando o motivo de no caso da classe de erro ele
-                 * ter pirado o cabeca e feito um
-                 * 	classCTD CorujaError implements CorujaErrorInterface
-                 * ao invez de
-                 * 	class CTDCorujaError implements CorujaErrorInterface
-                 */
-                $strOldInterfaceName = $arrWords[0];
-                $strNewInterfaceName = self::CODE_TO_DIAGRAM_CLASS_PREFIX . $strOldInterfaceName;
-
-                $arrOldInterface[] = $strOldInterfaceName;
-                $arrNewInterface[] = $strNewInterfaceName;
-
-                $arrWords[0] = $strNewInterfaceName;
-                $strAfter = implode( " " , $arrWords );
-                $strLine = $strBefore . $strAfter;
-                $arrLines[ $intLine ] = $strLine;
-            }
-        }
+        $this->codeInstrumentationClass( "class " , $arrOldClasses , $arrNewClasses, $arrLines );
+        $this->codeInstrumentationClass( "interface " , $arrOldInterface , $arrNewInterface, $arrLines );
 
         $strContentFile = implode( "\n" , $arrLines );
 
@@ -401,7 +384,35 @@ class CodeToDiagram
                 eval( $strNewCode );
             }
         }
+    }
 
+    public function loadFile( $strFileFrom, $strFile )
+    {
+        if( basename( $strFile ) == 'CodeToDiagram.class.php' )
+        {
+            return $this;
+        }
+        $this->addFile( $strFileFrom, $strFile );
+
+        if( $this->isRelativePath( $strFile ) )
+        {
+            $strFullFile = $this->fixFileName( $strFileFrom, $strFile );
+        }
+        else
+        {
+            $strFullFile = $strFile;
+        }
+
+        if( ! file_exists( $strFullFile ) )
+        {
+            throw new CodeToDiagramException( "Unable to find file: " . $strFullFile );
+        }
+
+        $strContentFile = file_get_contents( $strFullFile );
+        $strContentFile = $this->convertFileContent( $strContentFile , $strFile , $strFullFile );
+        $this->codeInstrumentationFile( $strFile, $strContentFile );
+        
+        return $this;
     }
 }
 ?>
