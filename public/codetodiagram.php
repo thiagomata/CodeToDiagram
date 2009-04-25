fe5244373a9b2c8b3c3de0be96627c7258cf25e0
blaine <blaine@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-25 00:00:16 +0000

    less constants and more attributes make the classes more flexible and more easy to be used in different contexts
    git-svn-id: http://subversion.assembla.com/svn/codetodiagram@56 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- public/codetodiagram.php
+++ public/codetodiagram.php
@@ -1,14 +1,18 @@
 <?php
+
+/**
+ * Active all the errors messages
+ */
 error_reporting( E_ALL | E_NOTICE | E_PARSE );
 ini_set( 'display_errors', 'On' );
 
-if( !defined("PUBLIC_PATH") )
+if( !isset( $strPublicPath  ) )
 {
     #1. Define the public path
-    define( "PUBLIC_PATH" , str_replace( "\\" , "/" , str_replace( basename( __FILE__ ) , "" , __FILE__ ) ) );
+    $strPublicPath =str_replace( "\\" , "/" , str_replace( basename( __FILE__ ) , "" , __FILE__ ) );
 
     #2. Load the components of the code to diagram
-    require_once( PUBLIC_PATH . '../components/_start.php' );
+    require_once( $strPublicPath . '../components/_start.php' );
 
     #3. redefine the error handler function to the code to diagram
 
@@ -25,15 +29,17 @@ if( !defined("PUBLIC_PATH") )
     #4. get the caller path
     $arrDebugBackTrace = debug_backtrace();
     $strFile = ( $arrDebugBackTrace[0][ 'file' ] );
-    define( "CALLER_PATH", str_replace( basename( $strFile ) , "" , $strFile ) );
+    $strCallerPath = ( str_replace( basename( $strFile ) , "" , $strFile ) );
 
     #5. get the relative path of the code to diagram bootstrap
-    $strPathFile = XmlSequence::getRelativePath( CALLER_PATH , PUBLIC_PATH );
+    $strPathFile = CorujaStringManipulation::getRelativePath( $strCallerPath , $strPublicPath );
 
     #6. add a instance of the code to diagram bootstrap into the load files array for the recursive call
     CodeToDiagram::getInstance()->addFile( $strPathFile . basename( __FILE__ ) , basename( __FILE__ ) );
 
     #7. init the code to diagram
     CodeToDiagram::init( basename( $strFile ) );
+    CodeToDiagram::getInstance()->setPublicPath( $strPublicPath );
+    CodeToDiagram::getInstance()->setCallerPath( $strCallerPath );
 }
 ?>
