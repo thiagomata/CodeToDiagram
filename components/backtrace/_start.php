82ff2403c6c00194bb334bb877364a598329b52b
blaine <blaine@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-28 12:44:03 +0000

    phpdoc to the backtrace
    git-svn-id: http://subversion.assembla.com/svn/codetodiagram@60 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- components/backtrace/_start.php
+++ components/backtrace/_start.php
@@ -2,6 +2,7 @@
 /**
  * Load the Backtrace package
  * @package backtrace
+ * @author Renan de Lima Barbosa <renandelima@gmail.com>
  */
 require_once( "Backtracephp" );
 require_once( "BackTraceExplain.php" );
