fcc028dd85c8525babafb886823b4c74007782c5
blaine <blaine@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-28 12:44:31 +0000

    phpdoc to the code reflection
    git-svn-id: http://subversion.assembla.com/svn/codetodiagram@61 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- components/codeReflection/_start.php
+++ components/codeReflection/_start.php
@@ -1,6 +1,8 @@
 <?php
 /**
  * Load the CodeReflection package
+ * @author Thiago Henrique Ramos da Mata <thiago.henrique.mata@gmail.com>
+ * @package CodeReflection
  */
 require_once( "CodeReflectionException.class.php" );
 require_once( "CodeReflectionFile.class.php" );
